f050fbb1aec3a1c608965cf903d5eadc2714d80d
Ricky Tang <tangricky001@gmail.com>
2025-08-07 16:34:38 -0400

    add q2375

---
+++ medium/2375-construct-smallest-number-from-di-string/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string smallestNumber(string pattern)
+    {
+        stack<int> st;
+        string ans;
+        for (int i = 0; i <= pattern.size(); ++i)
+        {
+            st.push(i + 1);
+
+            if (i == pattern.size() || pattern[i] == 'I')
+            {
+                while (!st.empty())
+                {
+                    ans += to_string(st.top());
+                    st.pop();
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    cout << "Hello C++" << endl;
+    return 0;
+}
