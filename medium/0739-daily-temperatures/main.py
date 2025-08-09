058ad570a91fb9d41142bc20c5405c599489e09b
Ricky Tang <tangricky001@gmail.com>
2025-08-09 18:00:15 -0400

    add q739

---
+++ medium/0739-daily-temperatures/main.py
@@ -0,0 +1,14 @@
+from typing import*
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        ans = [0] * len(temperatures)
+        st = []
+
+        for i in range(len(temperatures)):
+            while st and temperatures[i] > temperatures[st[-1]]:
+                index = st.pop()
+                ans[index] = i - index
+            st.append(i)
+        
+        return ans
