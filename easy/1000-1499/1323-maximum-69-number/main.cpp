12262cd633cf8621d75b6b31c094550ca6926706
Ricky Tang <tangricky001@gmail.com>
2025-08-15 22:04:15 -0400

    add q1323

---
+++ easy/1000-1499/1323-maximum-69-number/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maximum69Number(int num)
+    {
+        string s = to_string(num);
+        for (char &c : s)
+        {
+            if (c == '6')
+            {
+                c = '9';
+                break;
+            }
+        }
+        return stoi(s);
+    }
+};
+
+int main()
+{
+    Solution sol;
+    int num = 9669;
+    cout << sol.maximum69Number(num) << endl; // expected output: 9969
+    return 0;
+}
