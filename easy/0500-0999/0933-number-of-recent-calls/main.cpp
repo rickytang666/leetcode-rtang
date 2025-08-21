f040f09581c2190e64543460e59aad129fbe1ecb
Ricky Tang <tangricky001@gmail.com>
2025-08-20 22:04:15 -0400

    add q933

---
+++ easy/0500-0999/0933-number-of-recent-calls/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class RecentCounter
+{
+public:
+    RecentCounter()
+    {
+    }
+
+    int ping(int t)
+    {
+        q.push(t);
+        while (q.front() < t - 3000)
+        {
+            q.pop();
+        }
+        return q.size();
+    }
+
+private:
+    queue<int> q;
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
+
+int main()
+{
+    cout << "Hello C++" << endl;
+    return 0;
+}
