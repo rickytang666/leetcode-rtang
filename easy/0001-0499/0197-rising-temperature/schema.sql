ba38453d7bf3719921fd92912387c217d03cabc6
Ricky Tang <tangricky001@gmail.com>
2025-08-30 15:59:35 -0400

    add q197 (sql)

---
+++ easy/0001-0499/0197-rising-temperature/schema.sql
@@ -0,0 +1,13 @@
+Create table If Not Exists Weather (id int, recordDate date, temperature int)
+Truncate table Weather
+insert into Weather (id, recordDate, temperature) values ('1', '2015-01-01', '10')
+insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25')
+insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20')
+insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30')
+
+# Write your MySQL query statement below
+select w1.id
+from Weather w1
+join Weather w2
+on datediff(w1.recordDate, w2.recordDate) = 1
+where w1.temperature > w2.temperature;
