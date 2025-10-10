6138fec76b1d13e0631f68cc1c1a37a20e945ae5
Ricky Tang <tangricky001@gmail.com>
2025-10-10 14:32:24 -0400

    add q1967

---
+++ easy/1500-1999/1967-number-of-strings-that-appear-as-substrings-in-word/main.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def numOfStrings(self, patterns: List[str], word: str) -> int:
+        ans = 0
+        for p in patterns:
+            if p in word: ans += 1
+        return ans
