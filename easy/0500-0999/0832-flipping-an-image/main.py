d0fe4b90019dbbf25cfa3b4e0bcf6eeb828fb091
Ricky Tang <tangricky001@gmail.com>
2025-10-19 12:38:00 -0400

    add q832

---
+++ easy/0500-0999/0832-flipping-an-image/main.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        n = len(image)
+        for i in range(n):
+            image[i].reverse()
+            for j in range(n):
+                image[i][j] = image[i][j] ^ 1
+        return image
