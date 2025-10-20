21975240ac1646cc4573de2e9737b298a12aa760
Ricky Tang <tangricky001@gmail.com>
2025-10-20 12:58:00 -0400

    add q2011

---
+++ easy/2000-2499/2011-final-value-of-variable-after-performing-operations/main.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        x = 0
+        for o in operations:
+            if o == '++X' or o == 'X++':
+                x += 1
+            else:
+                x -= 1
+        return x
