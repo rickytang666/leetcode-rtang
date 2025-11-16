bd62049fc29694120dd0b7cfe8bc5a8221b0a8da
Ricky Tang <tangricky001@gmail.com>
2025-11-16 11:09:08 -0500

    add q901

---
+++ medium/0500-0999/0901-online-stock-span/main.py
@@ -0,0 +1,11 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.st = []
+
+    def next(self, price: int) -> int:
+        ans = 1
+        while self.st and self.st[-1][0] <= price:
+            ans += self.st.pop()[1]
+        self.st.append([price, ans])
+        return ans
