f6a4ead81a4d2d0a0a6742a652ce715cce5995eb
Ricky Tang <tangricky001@gmail.com>
2025-11-17 15:26:50 -0500

    add q1437

---
+++ easy/1000-1499/1437-check-if-all-1-s-are-at-least-length-k-places-away/main.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        last = -1
+        for i in range(len(nums)):
+            if nums[i]:
+                if last >= 0 and i - last - 1 < k:
+                    return False
+                last = i
+        return True
