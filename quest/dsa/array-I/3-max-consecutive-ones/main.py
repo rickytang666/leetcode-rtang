5206f863e95567ac7316f4476de9f06ae2dcf62d
Ricky Tang <tangricky001@gmail.com>
2025-11-20 14:11:54 -0500

    add quest dsa array1 q3

---
+++ quest/dsa/array-I/3-max-consecutive-ones/main.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        ans = 0
+        curr = 0
+        for i in range(len(nums)):
+            if nums[i]:
+                curr += 1
+            else:
+                ans = max(ans, curr)
+                curr = 0
+        ans = max(ans, curr)
+        return ans
