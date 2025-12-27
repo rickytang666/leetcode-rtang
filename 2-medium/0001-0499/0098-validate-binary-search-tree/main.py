1123944b79a863b65c7f567cc265af44918a0e11
Ricky Tang <tangricky001@gmail.com>
2025-12-26 23:22:36 -0500

    add q98

---
+++ 2-medium/0001-0499/0098-validate-binary-search-tree/main.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def rec(node, lo, hi):
+            if not node: return True
+            if node.val <= lo or node.val >= hi: return False
+            return rec(node.left, lo, node.val) and rec(node.right, node.val, hi)
+        return rec(root, float('-inf'), float('inf'))
