2968b59a06f949563873bd8bee580548e719014d
Ricky Tang <tangricky001@gmail.com>
2025-12-28 18:44:59 -0500

    add q144

---
+++ 1-easy/0001-0499/0144-binary-tree-preorder-traversal/main.py
@@ -0,0 +1,19 @@
+from typing import Optional, List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        self.ans = []
+        def rec(node):
+            if not node: return
+            self.ans.append(node.val)
+            rec(node.left)
+            rec(node.right)
+        rec(root)
+        return self.ans
