43f7ceebe16c8752fb0eb82cc28d4eb9a3712a2e
Ricky Tang <tangricky001@gmail.com>
2026-02-01 12:33:34 -0500

    add q133

---
+++ 2-medium/0001-0499/0133-clone-graph/main.py
@@ -0,0 +1,27 @@
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node: return None
+
+        visited = {}
+
+        def dfs(curr):
+            if curr in visited:
+                return visited[curr]
+            
+            clone = Node(curr.val)
+            visited[curr] = clone
+
+            for n in curr.neighbors:
+                clone.neighbors.append(dfs(n))
+
+            return clone
+        
+        return dfs(node)
