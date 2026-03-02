9d823636273f8c9680e104c734e68a0cd10ca466
Ricky Tang <tangricky001@gmail.com>
2026-03-02 10:00:21 -0500

    add q45

---
+++ 2-medium/0001-0499/0045-jump-game-ii/main.py
@@ -0,0 +1,18 @@
+from typing import List
+
+# greedy BFS
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        ans = 0
+        l = r = 0
+
+        while r < len(nums) - 1:
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+            l = r + 1
+            r = farthest
+            ans += 1
+        
+        return ans
