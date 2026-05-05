e5a65553646270425a36c291d18a102e61282d62
Ricky Tang <tangricky001@gmail.com>
2026-05-05 13:33:24 -0400

    add q450

---
+++ 2-medium/0001-0499/0450-delete-node-in-a-bst/main.py
@@ -0,0 +1,36 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        def findMin(node):
+            while node.left:
+                node = node.left
+            return node
+        
+        if not root:
+            return None
+        
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if (not root.left) and (not root.right):
+                return None
+            elif not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            else:
+                replacement = findMin(root.right)
+                root.val = replacement.val
+                root.right = self.deleteNode(root.right, replacement.val)
+        
+        return root
